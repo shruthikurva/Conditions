6640e7f76894f0a6d921ce4ca1d4aa7994593db0
shruthikurva <kurvashruthi98@gmail.com>
2025-01-30 21:24:53 +0530

    Add files via upload

---
+++ Conditions.java
@@ -0,0 +1,13 @@
+public class Conditions {
+    public static void main(String[] args) {
+        int num=8;
+        if (num>0){
+            System.out.println("The number is positive.");
+        }else if (num<0){
+                System.out.println("The number is negative.");
+            }else {
+                System.out.println("The number is zero.");
+            }
+       }
+    }
+ 
